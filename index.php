d465eee91ef645cda2d4bb359754c562233aca2f
Soorya-P <sooryapachakkil15@gmail.com>
2021-11-06 23:36:01 +0530

    Landing page of E-Ration shop(index page) - first section

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Soorya P">
+    <title>E-Ration_Shop</title>
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="icon" href="./image/icon/logo.png" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,700;1,200;1,300;1,400;1,500;1,600;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div class="hed1">
+            <img src="./image/icon/logo.png" />
+        </div>
+        <div class="hed2">
+            <h1>Aadhaar enabled Public Distribution System</h1>
+            <h4>Food, Civil Supplies and Consumer Affairs department Government of Kerala</h4>
+        </div>
+    </header>
+    <section>
+        <div class="section1">
+            <div class="set1-box">
+                <h1>E-Ration Shop Management</h1>
+                <h3>
+                    The Indian ration card provides food fo the poor people which is
+                    distributed by the government along with the fuel. It provides a
+                    distint identity of person which is useful to update with the
+                    government record. The basic food items provided by government
+                    are rice, sugar, wheat.
+                </h3>
+            </div>
+            <div class="set2-box">
+                <img src="./image/landing_page/items.png" />
+            </div>
+        </div>
+        <div class="section2">
+            <div class="set3-box box">
+                <img src="./image/landing_page/shop.png" />
+                <h3>Total shops</h3>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
